bea4a12fa3534d915caca9c2a21f686f8385812b
ramanuj-space <62047740+ramanuj-space@users.noreply.github.com>
2024-03-07 07:43:35 +0000

    conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -8,9 +8,34 @@
 // "33" => 33 
 // "33ada" => NAN
 // true = 1, false = 0
-let IsLoggedIn = 1
-let BoleanIsLoggedIn = Boolean(IsLoggedIn)
-console.log(BoleanIsLoggedIn)
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// let IsLoggedIn = 1
+// let BoleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(BoleanIsLoggedIn)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 ="hitesh"
+let str3 = str1 +str2 
+//console.log(str3);
+//console.log((3+4)*5%3)
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+// js mdn  prefix postfix
