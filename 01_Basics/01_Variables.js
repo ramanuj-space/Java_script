6f474305f3c185dd1b8b87d237e655fcccd3c12f
ramanuj-space <62047740+ramanuj-space@users.noreply.github.com>
2024-03-07 06:34:14 +0000

    variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 144556
+let accountEmail = "ram@google.com"
+var accountpass = "12345"
+accountCity = "jaipur"
+
+
+accountEmail = "shyam@dom.com"
+accountpass = "23113"
+accountCity = "bhopal"
+// accountId = 2 not allowed
+
+console.log(accountId);
+/*
+prefer not to use var
+*/
+
+console.table([accountId, accountEmail,accountpass,accountCity])
