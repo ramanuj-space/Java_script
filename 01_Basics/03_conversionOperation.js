0cfcdc020d8bc6f46e3033d154380ee5295ce60d
ramanuj-space <62047740+ramanuj-space@users.noreply.github.com>
2024-03-07 07:05:54 +0000

    conversion of datapype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+//let score = "33"
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(typeof score)
+// "33" => 33 
+// "33ada" => NAN
+// true = 1, false = 0
+let IsLoggedIn = 1
+let BoleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(BoleanIsLoggedIn)
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
