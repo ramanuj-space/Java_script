8c50335c01fb61668bf6a2a2ed6b0b4b78d0976e
ramanuj-space <62047740+ramanuj-space@users.noreply.github.com>
2024-03-07 08:26:26 +0000

    datatype summry

---
+++ 01_Basics/datatype-summry.js
@@ -0,0 +1,23 @@
+// primitive
+//  7 types :
+// string , number ,boolean, null, undefined,symbol,bigint
+
+const score = 100
+const scoreValue = 100.3
+const IsLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherID = Symbol('123s')
+const BigNumber = 468765498854657456n
+
+// refrence (non premitive)
+//arry object , function
+//
+const heros =["saktiman","naagraj","doga"];
+let muObj = {name : "ram" ,age : 33}
+const myfunction = function(){
+    console.log("hello world");
+
+    
+}
